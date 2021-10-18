b9eb72add5095f58b60c9fa4e9095e2ea494e38d
ngaodz123 <84268783+ngaodz123@users.noreply.github.com>
2021-10-19 00:21:12 +0700

    Create bai2.pas

---
+++ bai2.pas
@@ -0,0 +1,31 @@
+program bai2;
+
+users crt;
+
+begin
+
+  clrscr;
+
+  writeln('16/3 =', 16/3);
+
+  delay(5000);
+
+  writeln('16 div 3 =', 16 div 3);
+
+  delay(5000);
+
+  writeln('16 mod 3 =', 16 mov 3);
+
+  delay(5000);
+
+  writeln('16 mod 3 =', 16-(16 div 3)*3);
+
+  delay(5000);
+
+  writeln('16 div 3 =', 16-(16 mod 3)/3);
+
+  writeln('Nguyễn Phương Anh')
+
+  readln;
+
+end.
