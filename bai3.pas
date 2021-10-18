3cb0b2d7cb9c9e9ce0cc3a42b310e2d67ac083c4
ngaodz123 <84268783+ngaodz123@users.noreply.github.com>
2021-10-19 00:21:39 +0700

    Create bai3.pas

---
+++ bai3.pas
@@ -0,0 +1,21 @@
+program bai3;
+
+users crt;
+
+begin
+
+  clrscr;
+
+  writeln('(15 * 4) - (30 + 12) =', (15 * 4) - (30 + 12));
+
+  writeln((10 +5) / (3 + 1) - 18 / (5 + 1):4:2);
+
+  writeln((10 + 2) * (10 + 2) / (3 + 1):4:2);
+
+  writeln(((10 + 2) * (10 + 2) - 24)) / ( 3 + 1):4:2;
+
+  writeln('Nguyễn Phương Anh')
+
+  readln;
+
+ end.
